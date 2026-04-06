dc0bb6f6f0500703094aa2698b2332a20eac69f5
pfilip04 <filip0309kg@gmail.com>
2026-04-06 19:46:17 +0200

    nand and adder finished, full adder started

--- circuit.js
+++ circuit.js
@@ -163,7 +163,7 @@ switchB3.addEventListener('click', () => {
 });
 
 
-// Circuit 4: XOR gate
+// Circuit 4: NAND gate
 
 const switchA4 = document.getElementById('switchA4');
 const switchB4 = document.getElementById('switchB4');
@@ -173,66 +173,30 @@ function updateCircuit4() {
 
     const a = switchA4.classList.contains('on');
     const b = switchB4.classList.contains('on');
+    const out1 = a && b;
 
-    const outA1 = !a;
-    const outB1 = !b;
-
-    const outA2 = a && outB1;
-    const outB2 = outA1 && b;
-
-    const out = outA2 || outB2;
+    const out = !out1;
 
     setActive('wireB4', a);
     setActive('wireB4_2', a);
-    setActive('wireC4', a);
-    setActive('wireC4_2', a);
-    setActive('wireN4', a);
-    setActive('wireN4_2', a);
-    setActive('wireConnectorA4', a);
-    setActive('wireOutA4', a);
-    setActive('wireOutA4_2', a);
-    setActive('wireOutG4', a);
-    setActive('wireOutG4_2', a);
 
-    setActive('wireJ4', b);
-    setActive('wireJ4_2', b);
-    setActive('wireF4', b);
-    setActive('wireF4_2', b);
-    setActive('wireConnectorB4', b);
-    setActive('wireOutC4', b);
-    setActive('wireOutC4_2', b);
+    setActive('wireD4', b);
+    setActive('wireD4_2', b);
 
+    setActive('wireA4', out1);
+    setActive('wireA4_2', out1);
+    setActive('wireOutA4', out1);
+    setActive('wireOutA4_2', out1);
+    setActive('wireC4', out1);
+    setActive('wireC4_2', out1);
+    setActive('wireOutB4', out1);
+    setActive('wireOutB4_2', out1);
 
-    setActive('wireL4', outA1);
-    setActive('wireL4_2', outA1);
-    setActive('wireOutF4', outA1);
-    setActive('wireOutF4_2', outA1);
-    setActive('wireI4', outA1);
-    setActive('wireI4_2', outA1);
+    setActive('wireSourceB4', out);
+    setActive('wireSourceB4_2', out);
+    setActive('wireOutC4', out);
+    setActive('wireOutC4_2', out);
 
-    setActive('wireD4', outB1);
-    setActive('wireD4_2', outB1);
-
-    setActive('wireH4', outA2);
-    setActive('wireH4_2', outA2);
-    setActive('wireOutD4', outA2);
-    setActive('wireOutD4_2', outA2);
-    setActive('wireOutB4', outA2);
-    setActive('wireOutB4_2', outA2);
-    setActive('wireOutI4', outA2);
-
-    setActive('wireP4', outB2);
-    setActive('wireP4_2', outB2);
-    setActive('wireOutH4', outB2);
-    setActive('wireOutH4_2', outB2);
-    setActive('wireOutE4', outB2);
-    setActive('wireOutE4_2', outB2);
-    setActive('wireOutI4_2', outB2);
-
-    setActive('wireOutI4_3', out);
-    setActive('wireOutJ4', out);
-    setActive('wireOutJ4_2', out);
-    setActive('wireOutJ4_3', out);
 
     lamp4.classList.toggle('on', out);
 }
@@ -249,20 +213,166 @@ switchB4.addEventListener('click', () => {
     updateCircuit4();
 });
 
-// Circuit 5: Binary Decoder
 
-const switchP5 = document.getElementById('switchP5');
-const switchQ5 = document.getElementById('switchQ5');
+// Circuit 5: XOR gate
 
-const lampA5 = document.getElementById('lampA5');
-const lampB5 = document.getElementById('lampB5');
-const lampC5 = document.getElementById('lampC5');
-const lampD5 = document.getElementById('lampD5');
+const switchA5 = document.getElementById('switchA5');
+const switchB5 = document.getElementById('switchB5');
+const lamp5 = document.getElementById('lamp5');
 
 function updateCircuit5() {
 
-    const p = switchP5.classList.contains('on');
-    const q = switchQ5.classList.contains('on');
+    const a = switchA5.classList.contains('on');
+    const b = switchB5.classList.contains('on');
+
+    const outA1 = !a;
+    const outB1 = !b;
+
+    const outA2 = a && outB1;
+    const outB2 = outA1 && b;
+
+    const out = outA2 || outB2;
+
+    setActive('wireB5', a);
+    setActive('wireB5_2', a);
+    setActive('wireC5', a);
+    setActive('wireC5_2', a);
+    setActive('wireN5', a);
+    setActive('wireN5_2', a);
+    setActive('wireConnectorA5', a);
+    setActive('wireOutA5', a);
+    setActive('wireOutA5_2', a);
+    setActive('wireOutG5', a);
+    setActive('wireOutG5_2', a);
+
+    setActive('wireJ5', b);
+    setActive('wireJ5_2', b);
+    setActive('wireF5', b);
+    setActive('wireF5_2', b);
+    setActive('wireConnectorB5', b);
+    setActive('wireOutC5', b);
+    setActive('wireOutC5_2', b);
+
+
+    setActive('wireL5', outA1);
+    setActive('wireL5_2', outA1);
+    setActive('wireOutF5', outA1);
+    setActive('wireOutF5_2', outA1);
+    setActive('wireI5', outA1);
+    setActive('wireI5_2', outA1);
+
+    setActive('wireD5', outB1);
+    setActive('wireD5_2', outB1);
+
+    setActive('wireH5', outA2);
+    setActive('wireH5_2', outA2);
+    setActive('wireOutD5', outA2);
+    setActive('wireOutD5_2', outA2);
+    setActive('wireOutB5', outA2);
+    setActive('wireOutB5_2', outA2);
+    setActive('wireOutI5', outA2);
+
+    setActive('wireP5', outB2);
+    setActive('wireP5_2', outB2);
+    setActive('wireOutH5', outB2);
+    setActive('wireOutH5_2', outB2);
+    setActive('wireOutE5', outB2);
+    setActive('wireOutE5_2', outB2);
+    setActive('wireOutI5_2', outB2);
+
+    setActive('wireOutI5_3', out);
+    setActive('wireOutJ5', out);
+    setActive('wireOutJ5_2', out);
+    setActive('wireOutJ5_3', out);
+
+    lamp5.classList.toggle('on', out);
+}
+
+switchA5.addEventListener('click', () => {
+
+    switchA5.classList.toggle('on');
+    updateCircuit5();
+});
+
+switchB5.addEventListener('click', () => {
+
+    switchB5.classList.toggle('on');
+    updateCircuit5();
+});
+
+// Circuit 6: ADDER
+
+const switchA6 = document.getElementById('switchA6');
+const switchB6 = document.getElementById('switchB6');
+const lampA6 = document.getElementById('lampA6');
+const lampB6 = document.getElementById('lampB6');
+
+function updateCircuit6() {
+
+    const a = switchA6.classList.contains('on');
+    const b = switchB6.classList.contains('on');
+
+    setActive('NumA6', a);
+    setActive('NumB6', b);
+    setContent('NumA6', Number(a));
+    setContent('NumB6', Number(b));
+
+    const out1 = a && b;
+    const out2= a ^ b;
+
+    setActive('NumInA6', a);
+    setActive('NumInB6', b);
+    setContent('NumInA6', Number(a));
+    setContent('NumInB6', Number(b));
+
+    setActive('wireA6', a);
+    setActive('wireA6_2', a);
+    setActive('wireA6_3', a);
+
+    setActive('wireB6', b);
+    setActive('wireB6_2', b);
+    setActive('wireB6_3', b);
+
+    setActive('wireOutA6', out1);
+    setActive('wireOutA6_2', out1);
+    setActive('NumOutA6', out1);
+    setContent('NumOutA6', Number(out1));
+
+    setActive('wireOutB6', out2);
+    setActive('wireOutB6_2', out2);
+    setActive('NumOutB6', out2);
+    setContent('NumOutB6', Number(out2));
+
+    lampA6.classList.toggle('on', out1);
+    lampB6.classList.toggle('on', out2);
+}
+
+switchA6.addEventListener('click', () => {
+
+    switchA6.classList.toggle('on');
+    updateCircuit6();
+});
+
+switchB6.addEventListener('click', () => {
+
+    switchB6.classList.toggle('on');
+    updateCircuit6();
+});
+
+// Circuit 8: Binary Decoder
+
+const switchP8 = document.getElementById('switchP8');
+const switchQ8 = document.getElementById('switchQ8');
+
+const lampA8 = document.getElementById('lampA8');
+const lampB8 = document.getElementById('lampB8');
+const lampC8 = document.getElementById('lampC8');
+const lampD8 = document.getElementById('lampD8');
+
+function updateCircuit8() {
+
+    const p = switchP8.classList.contains('on');
+    const q = switchQ8.classList.contains('on');
 
     const notP = !p;
     const notQ = !q;
@@ -272,108 +382,108 @@ function updateCircuit5() {
     const out3 = p && notQ;
     const out4 = p && q;
 
-    setActive('wireP5', p);
-    setActive('wireP5_2', p);
-    setActive('wireInP5', p);
-    setActive('wireInP5_2', p);
-    setActive('wireInP5_3', p);
+    setActive('wireP8', p);
+    setActive('wireP8_2', p);
+    setActive('wireInP8', p);
+    setActive('wireInP8_2', p);
+    setActive('wireInP8_3', p);
 
-    setActive('wireQ5', q);
-    setActive('wireQ5_2', q);
-    setActive('wireInQ5', q);
-    setActive('wireInQ5_2', q);
-    setActive('wireInQ5_3', q);
+    setActive('wireQ8', q);
+    setActive('wireQ8_2', q);
+    setActive('wireInQ8', q);
+    setActive('wireInQ8_2', q);
+    setActive('wireInQ8_3', q);
 
-    setActive('wireInN1P5', notP);
-    setActive('wireInN2P5', notP);
-    setActive('wireInN2P5_2', notP);
+    setActive('wireInN1P8', notP);
+    setActive('wireInN2P8', notP);
+    setActive('wireInN2P8_2', notP);
 
-    setActive('wireInN1Q5', notQ);
-    setActive('wireInN2Q5', notQ);
-    setActive('wireInN2Q5_2', notQ);
+    setActive('wireInN1Q8', notQ);
+    setActive('wireInN2Q8', notQ);
+    setActive('wireInN2Q8_2', notQ);
 
-    setActive('wireOutA5', out1);
-    setActive('wireOutB5', out2);
-    setActive('wireOutC5', out3);
-    setActive('wireOutD5', out4);
+    setActive('wireOutA8', out1);
+    setActive('wireOutB8', out2);
+    setActive('wireOutC8', out3);
+    setActive('wireOutD8', out4);
 
-    setContent('NumA5', Number(p))
-    setContent('NumB5', Number(q))
+    setContent('NumA8', Number(p));
+    setContent('NumB8', Number(q));
 
-    lampA5.classList.toggle('on', out1);
-    lampB5.classList.toggle('on', out2);
-    lampC5.classList.toggle('on', out3);
-    lampD5.classList.toggle('on', out4);
+    lampA8.classList.toggle('on', out1);
+    lampB8.classList.toggle('on', out2);
+    lampC8.classList.toggle('on', out3);
+    lampD8.classList.toggle('on', out4);
 }
 
-switchP5.addEventListener('click', () => {
+switchP8.addEventListener('click', () => {
 
-    switchP5.classList.toggle('on');
-    updateCircuit5();
+    switchP8.classList.toggle('on');
+    updateCircuit8();
 });
 
-switchQ5.addEventListener('click', () => {
+switchQ8.addEventListener('click', () => {
 
-    switchQ5.classList.toggle('on');
-    updateCircuit5();
+    switchQ8.classList.toggle('on');
+    updateCircuit8();
 });
 
-// Circuit 6: SET-RESET circuit
+// Circuit 9: SET-RESET circuit
 
-const switchA6 = document.getElementById('switchA6');
-const switchB6 = document.getElementById('switchB6');
+const switchA9 = document.getElementById('switchA9');
+const switchB9 = document.getElementById('switchB9');
 
-function updateCircuit6() {
+function updateCircuit9() {
 
-    const a = switchA6.classList.contains('on');
-    const b = switchB6.classList.contains('on');
-    const c = getActive('wireInNOut6');
+    const a = switchA9.classList.contains('on');
+    const b = switchB9.classList.contains('on');
+    const c = getActive('wireInNOut9');
 
     const out1 = a || c;
     const out2 = !b;
-    const out = out1 && out2
+    const out = out1 && out2;
 
-    setActive('wireA6', a);
-    setActive('wireB6', b);
+    setActive('wireA9', a);
+    setActive('wireB9', b);
 
-    setActive('wireOutA6', out1);
-    setActive('wireOutB6', out2);
+    setActive('wireOutA9', out1);
+    setActive('wireOutB9', out2);
 
-    setActive('wireOutX6', out);
-    setActive('wireOutX6_2', out);
-    setActive('wireOutX6_3', out);
-    setActive('wireOutX6_4', out);
-    setActive('wireInNOut6', out);
+    setActive('wireOutX9', out);
+    setActive('wireOutX9_2', out);
+    setActive('wireOutX9_3', out);
+    setActive('wireOutX9_4', out);
+    setActive('wireInNOut9', out);
     
-    setActive('Out6', out);
-    setContent('Out6', Number(out));
+    setActive('Out9', out);
+    setContent('Out9', Number(out));
 }
 
-switchA6.addEventListener('click', () => {
+switchA9.addEventListener('click', () => {
 
-    switchA6.classList.toggle('on');
-    updateCircuit6();
-    updateCircuit6();
+    switchA9.classList.toggle('on');
+    updateCircuit9();
+    updateCircuit9();
 });
 
-switchB6.addEventListener('click', () => {
+switchB9.addEventListener('click', () => {
 
-    switchB6.classList.toggle('on');
-    updateCircuit6();
-    updateCircuit6();
+    switchB9.classList.toggle('on');
+    updateCircuit9();
+    updateCircuit9();
 });
 
-// Circuit 7: WRITE ENABLE circuit
+// Circuit 10: WRITE ENABLE circuit
 
-const switchA7 = document.getElementById('switchA7');
-const switchB7 = document.getElementById('switchB7');
-const lamp7 = document.getElementById('lamp7');
+const switchA10 = document.getElementById('switchA10');
+const switchB10 = document.getElementById('switchB10');
+const lamp10 = document.getElementById('lamp10');
 
-function updateCircuit7() {
+function updateCircuit10() {
 
-    const a = switchA7.classList.contains('on');
-    const b = switchB7.classList.contains('on');
-    const c = getActive('wireInNOut7');
+    const a = switchA10.classList.contains('on');
+    const b = switchB10.classList.contains('on');
+    const c = getActive('wireInNOut10');
 
     const notA = !a;
 
@@ -384,46 +494,46 @@ function updateCircuit7() {
     const out4 = !out2;
     const out = out3 && out4;
 
-    setActive('wireA7', a);
-    setActive('wireConnA7', a);
-    setActive('wireConnA7_2', a);
+    setActive('wireA10', a);
+    setActive('wireConnA10', a);
+    setActive('wireConnA10_2', a);
 
-    setActive('wireB7', b);
-    setActive('wireConnB7', b);
-    setActive('wireConnB7_2', b);
+    setActive('wireB10', b);
+    setActive('wireConnB10', b);
+    setActive('wireConnB10_2', b);
 
-    setActive('wireOutNA7', notA);
+    setActive('wireOutNA10', notA);
 
-    setActive('wireOutA7', out1);
-    setActive('wireOutB7', out2);
+    setActive('wireOutA10', out1);
+    setActive('wireOutB10', out2);
 
-    setActive('wireOutC7', out3);
-    setActive('wireOutD7', out4);
+    setActive('wireOutC10', out3);
+    setActive('wireOutD10', out4);
 
-    setActive('wireOutX7', out);
-    setActive('wireOutX7_2', out);
-    setActive('wireOutX7_3', out);
-    setActive('wireOutX7_4', out);
+    setActive('wireOutX10', out);
+    setActive('wireOutX10_2', out);
+    setActive('wireOutX10_3', out);
+    setActive('wireOutX10_4', out);
 
-    setActive('wireInNOut7', out);
+    setActive('wireInNOut10', out);
     
-    setActive('Out7', out);
-    setContent('Out7', Number(out));
+    setActive('Out10', out);
+    setContent('Out10', Number(out));
 
-    setActive('wireOut7', out);
-    lamp7.classList.toggle('on', out);
+    setActive('wireOut10', out);
+    lamp10.classList.toggle('on', out);
 }
 
-switchA7.addEventListener('click', () => {
+switchA10.addEventListener('click', () => {
 
-    switchA7.classList.toggle('on');
-    updateCircuit7();
-    updateCircuit7();
+    switchA10.classList.toggle('on');
+    updateCircuit10();
+    updateCircuit10();
 });
 
-switchB7.addEventListener('click', () => {
+switchB10.addEventListener('click', () => {
 
-    switchB7.classList.toggle('on');
-    updateCircuit7();
-    updateCircuit7();
+    switchB10.classList.toggle('on');
+    updateCircuit10();
+    updateCircuit10();
 });
